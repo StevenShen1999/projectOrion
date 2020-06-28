2bd5ae3028849981e0b282c6438cdf2c81be1600
Steven Shen <stevenshen1999@hotmail.com>
2020-06-28 16:37:38 +0930

    Finished the navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-
+    <navbar />
   </div>
 </template>
 
 <script>
+import navbar from './components/navbar'
+
 export default {
   name: 'app',
+  components: {
+    navbar
+  },
   data () {
     return {
     }
---
+++ frontend/src/components/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Orion</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsedNavbar">
+                <span class="navbar-toggler-icon"/>
+            </button>
+
+            <div class="collapse navbar-collapse" id="collapsedNavbar">
+                <ul class="nav navbar-nav ml-auto">
+                    <a class="nav-item nav-link" href="#">Past Projects</a>
+                    <a class="nav-item nav-link" href="#">Resume</a>
+                    <a class="nav-item nav-link" href="#">About Me</a>
+                    <a class="nav-item nav-link" href="#">Get In Touch</a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'navbar'
+}
+</script>
